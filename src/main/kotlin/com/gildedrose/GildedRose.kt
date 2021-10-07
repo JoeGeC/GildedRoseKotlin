0ba51a19f1bdff9e5cde5d1093971de76e99dba7
Joe Barker <joe.barker@mkodo.com>
2021-10-07 10:55:28 +0100

    Refactored strings

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,12 +1,15 @@
 package com.gildedrose
 
 class GildedRose(var items: Array<Item>) {
+    private val agedBrie = "Aged Brie"
+    private val sulfuras = "Sulfuras, Hand of Ragnaros"
+    private val backstage = "Backstage passes to a TAFKAL80ETC concert"
 
     fun updateQuality() {
         items.forEach { item ->
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (item.name != agedBrie && item.name != backstage) {
                 if (item.quality > 0) {
-                    if (item.name != "Sulfuras, Hand of Ragnaros") {
+                    if (item.name != sulfuras) {
                         item.quality = item.quality - 1
                     }
                 }
@@ -14,7 +17,7 @@ class GildedRose(var items: Array<Item>) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1
 
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.name == backstage) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1
@@ -30,15 +33,15 @@ class GildedRose(var items: Array<Item>) {
                 }
             }
 
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
+            if (item.name != sulfuras) {
                 item.sellIn = item.sellIn - 1
             }
 
             if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.name != agedBrie) {
+                    if (item.name != backstage) {
                         if (item.quality > 0) {
-                            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                            if (item.name != sulfuras) {
                                 item.quality = item.quality - 1
                             }
                         }
